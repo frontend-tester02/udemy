a84939a638b2a2288318aa02c14f41795932e34a
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-08-22 15:06:15 +0500

    Change logo name

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
 	return (
 		<Link href={'/'} className='flex items-center gap-2'>
 			<Image src={'/logo.svg'} alt='Logo' width={50} height={50} />
-			<h1 className='font-spaceGrotesk text-4xl font-bold'>Sammi</h1>
+			<h1 className='font-spaceGrotesk text-4xl font-bold'>Udemy</h1>
 		</Link>
 	)
 }
