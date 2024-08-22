4d05b0435976e74d76c24f618150dfa3e2a31efc
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-08-22 14:49:08 +0500

    Add mode-toggle page

---
+++ components/shared/mode-toggle.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { Button } from '../ui/button'
+import { Moon, Sun } from 'lucide-react'
+
+function ModeToggle() {
+	const [mount, setMount] = useState(false)
+	const { setTheme, resolvedTheme } = useTheme()
+
+	useEffect(() => setMount(true), [])
+	return mount && resolvedTheme === 'dark' ? (
+		<Button size={'icon'} variant={'ghost'} onClick={() => setTheme('light')}>
+			<Sun />
+		</Button>
+	) : (
+		<Button size={'icon'} onClick={() => setTheme('dark')} variant={'ghost'}>
+			<Moon />
+		</Button>
+	)
+}
+
+export default ModeToggle
