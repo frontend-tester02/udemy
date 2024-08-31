9fbed2c3671f9f20eba9decb299657a38d683d79
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-01 02:11:39 +0900

    Add filterCourses and levels, and courses

--- constants/index.ts
+++ constants/index.ts
@@ -30,3 +30,98 @@ export const companies = [
 	DiIcons.DiDjango,
 	DiIcons.DiDocker,
 ]
+
+export const filterCourses = [
+	{ label: 'cateogry1', name: 'all' },
+	{ label: 'cateogry2', name: 'newest' },
+	{ label: 'cateogry3', name: 'lowest-price' },
+	{ label: 'cateogry4', name: 'highest-price' },
+]
+
+export const filterLevels = [
+	{ label: 'level1', name: 'all' },
+	{ label: 'level2', name: 'beginner' },
+	{ label: 'level3', name: 'intermediate' },
+	{ label: 'level4', name: 'advanced' },
+]
+
+export const courses = [
+	{
+		title: 'JavaScript',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F35ca3db9-fb43-4f12-bd48-8b08a503db09-kilwwj.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 179,
+		currentPrice: 79,
+		level: 'Beginner',
+	},
+	{
+		title: 'ReactJS',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2Fa8573b7c-95b2-4459-8414-8eacde874b0a-kilwdl.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 159,
+		currentPrice: 59,
+		level: 'Intermidate',
+	},
+	{
+		title: 'VueJS',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2Fbddfc919-2260-46dd-a078-b956bd9a377c-kilwcq.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 129,
+		currentPrice: 29,
+		level: 'Intermidate',
+	},
+	{
+		title: 'Telegram BOT',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F1662922d-b61d-401b-aa1a-693a6231d8a0-kilw9a.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 209,
+		currentPrice: 109,
+		level: 'Intermidate',
+	},
+	{
+		title: 'React Native',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2F27f17594-ae28-4fe1-86ce-964a5c89c78d-kilw7k.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 129,
+		currentPrice: 29,
+		level: 'Intermidate',
+	},
+	{
+		title: 'Foundation',
+		previewImage:
+			'https://sammi.ac/_next/image?url=https%3A%2F%2Futfs.io%2Ff%2Fbcdfb541-3300-430f-b8b2-ff0fb57df056-kilw6p.png&w=1920&q=75',
+		author: {
+			image:
+				'https://blog.sammi.app/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FKmuGvX9oRjYkG3bzttvc&w=1920&q=75',
+			name: 'Chris Impley',
+		},
+		oldPrice: 209,
+		currentPrice: 109,
+		level: 'Intermidate',
+	},
+]
