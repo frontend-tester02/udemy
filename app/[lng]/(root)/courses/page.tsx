c343e7f456f0a9514025814757c85ef6b1fd43d3
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-03 01:08:44 +0900

    Add all acourses page

--- app/[lng]/(root)/courses/page.tsx
+++ app/[lng]/(root)/courses/page.tsx
@@ -1,9 +1,11 @@
 import TopBar from '@/components/shared/top-bar'
+import AllCourses from './_components/all-courses'
 
 function Page() {
 	return (
 		<>
 			<TopBar label='allCourses' description='allCourseDescription' />
+			<AllCourses />
 		</>
 	)
 }
