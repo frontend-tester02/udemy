b401513cb3f128f46564fef70c78fbe2a01378c4
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-07 02:52:23 +0900

    Add review page to instructor

---
+++ app/[lng]/instructor/reviews/page.tsx
@@ -0,0 +1,27 @@
+import { Separator } from '@/components/ui/separator'
+import Header from '../_components/header'
+import InstructorReviewCard from '@/components/cards/instructor-review.card'
+
+function Page() {
+	return (
+		<>
+			<Header
+				title='Review'
+				description='Here are you can see all the reviews of your courses'
+			/>
+
+			<div className='mt-4 rounded-md bg-background p-4'>
+				<h3 className='font-spaceGrotesk text-lg font-medium'>All Reviewa</h3>
+				<Separator className='my-3' />
+
+				<div className='flex flex-col space-y-3'>
+					<InstructorReviewCard />
+					<InstructorReviewCard />
+					<InstructorReviewCard />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Page
