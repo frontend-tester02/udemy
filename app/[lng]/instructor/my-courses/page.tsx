d9449335deafa398f4feef3d88ce22e3d90d303f
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-09 23:48:51 +0900

    Add getCourses actions

--- app/[lng]/instructor/my-courses/page.tsx
+++ app/[lng]/instructor/my-courses/page.tsx
@@ -1,14 +1,18 @@
-import { courses } from '@/constants'
 import Header from '../_components/header'
 import InstructorCourseCard from '@/components/cards/instructor-course.card'
+import { getCourses } from '@/actions/course.action'
 
-function Page() {
+async function Page() {
+	const courses = await getCourses()
 	return (
 		<>
 			<Header title='My courses' description='Here are your latest courses' />
 			<div className='mt-4 grid grid-cols-3 gap-4'>
 				{courses.map(course => (
-					<InstructorCourseCard key={course.title} {...course} />
+					<InstructorCourseCard
+						key={course._id}
+						course={JSON.parse(JSON.stringify(course))}
+					/>
 				))}
 			</div>
 		</>
