f17e38806c2739af33a9a5c737c70411c62835f1
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-12 02:24:50 +0900

    Add slug type

--- database/course.model.ts
+++ database/course.model.ts
@@ -14,6 +14,7 @@ const CourseSchema = new Schema(
 		previewImage: String,
 		published: { type: Boolean, default: false },
 		instructor: { type: Schema.Types.ObjectId, ref: 'User' },
+		slug: String,
 	},
 	{ timestamps: true }
 )
