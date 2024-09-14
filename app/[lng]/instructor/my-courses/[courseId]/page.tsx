b48e3adc0c9b8659126b7d9c8e0e8d31cee82099
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-15 02:03:10 +0900

    Add sections, price and previewImage features

--- app/[lng]/instructor/my-courses/[courseId]/page.tsx
+++ app/[lng]/instructor/my-courses/[courseId]/page.tsx
@@ -2,11 +2,14 @@ import { getCourseById } from '@/actions/course.action'
 import Header from '../../_components/header'
 import Actions from './_components/actions'
 import { Separator } from '@/components/ui/separator'
-import { Settings } from 'lucide-react'
+import { Gem, Images, LayoutPanelLeft, Settings } from 'lucide-react'
 import CourseFields from './_components/course-fields'
 import Description from './_components/description'
 import Information from './_components/information'
 import SelectFields from './_components/select-fields'
+import Sections from './_components/sections'
+import Price from './_components/price'
+import PreviewImage from './_components/preview-image'
 
 async function Page({ params }: { params: { courseId: string } }) {
 	const courseJSON = await getCourseById(params.courseId)
@@ -39,7 +42,34 @@ async function Page({ params }: { params: { courseId: string } }) {
 					<Information {...course} />
 					<SelectFields {...course} />
 				</div>
-				<div className='flex flex-col space-y-2'></div>
+				<div className='flex flex-col space-y-2'>
+					{/* Sections */}
+					<div className='flex items-center gap-2'>
+						<span className='font-spaceGrotesk text-3xl font-medium'>
+							Course Sections
+						</span>
+						<LayoutPanelLeft />
+					</div>
+					<Sections {...course} />
+
+					{/* Price */}
+					<div className='flex items-center gap-2'>
+						<span className='font-spaceGrotesk text-3xl font-medium'>
+							Course Price
+						</span>
+						<Gem />
+					</div>
+					<Price {...course} />
+
+					{/* Preview Image */}
+					<div className='flex items-center gap-2'>
+						<span className='font-spaceGrotesk text-3xl font-medium'>
+							Preview Images
+						</span>
+						<Images />
+					</div>
+					<PreviewImage {...course} />
+				</div>
 			</div>
 		</>
 	)
