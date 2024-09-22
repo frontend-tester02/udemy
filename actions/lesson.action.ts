427b7da44cfdbf0b40ff9bc9a5ce0c0830121f52
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-22 22:43:52 +0900

    Add editLesson action

--- actions/lesson.action.ts
+++ actions/lesson.action.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { connectToDatabase } from '@/lib/mongoose'
-import { ICreateLesson } from './types'
+import { ICreateLesson, ILessonFields } from './types'
 import Section from '@/database/section.model'
 import Lesson from '@/database/lesson.model'
 import { revalidatePath } from 'next/cache'
@@ -52,6 +52,25 @@ export const deleteLesson = async (id: string, path: string) => {
 		section.lessons.pull(id)
 		section.save()
 		await Lesson.findByIdAndDelete(id)
+		revalidatePath(path)
+	} catch (error) {
+		throw new Error('Something went wrong!')
+	}
+}
+
+export const editLesson = async (
+	lesson: ILessonFields,
+	lessonId: string,
+	path: string
+) => {
+	try {
+		await connectToDatabase()
+		const duration = {
+			hours: Number(lesson.hours),
+			minutes: Number(lesson.minutes),
+			seconds: Number(lesson.seconds),
+		}
+		await Lesson.findByIdAndUpdate(lessonId, { ...lesson, duration })
 		revalidatePath(path)
 	} catch (error) {
 		throw new Error('Something went wrong!')
