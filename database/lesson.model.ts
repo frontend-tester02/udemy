319d944220c6a5f936d5d55b4cafca591d37040d
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-23 22:46:53 +0900

    Add Schema of free lesson model

--- database/lesson.model.ts
+++ database/lesson.model.ts
@@ -1,18 +1,19 @@
-import { model, models, Schema } from "mongoose";
+import { model, models, Schema } from 'mongoose'
 
 const LessonSchema = new Schema({
-    title:String,
-    position: Number,
-    content: String,
-    videoUrl:String,
-    duration:{
-        hours:{type:Number, default:0},
-        minutes:{type:Number, default:0},
-        seconds:{type:Number, default:0},
-    },
-    section:{type:Schema.Types.ObjectId, ref:"Section"},
+	title: String,
+	position: Number,
+	content: String,
+	videoUrl: String,
+	duration: {
+		hours: { type: Number, default: 0 },
+		minutes: { type: Number, default: 0 },
+		seconds: { type: Number, default: 0 },
+	},
+	section: { type: Schema.Types.ObjectId, ref: 'Section' },
+	free: { type: Boolean, default: false },
 })
 
-const Lesson = models.Lesson || model("Lesson", LessonSchema)
+const Lesson = models.Lesson || model('Lesson', LessonSchema)
 
 export default Lesson
