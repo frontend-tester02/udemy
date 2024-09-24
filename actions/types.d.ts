ca729f8fe791b125ae8afd74f608cc88f6a3396c
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-25 01:21:34 +0900

    Updated IUpdateUser types

--- actions/types.d.ts
+++ actions/types.d.ts
@@ -1,3 +1,5 @@
+import { IUser } from '@/app.types'
+
 export interface ICreateCourse {
 	title: string
 	description: string
@@ -20,11 +22,8 @@ export interface ICreateUser {
 
 export interface IUpdateUser {
 	clerkId: string
-	updatedData: {
-		fullName: string
-		email: string
-		picture: string
-	}
+	path: string
+	updatedData: Partial<IUser>
 }
 
 export interface IUpdateSection {
