229dfd4c9300d1279928157af36d7d0d914b2822
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-25 01:25:03 +0900

    Updated UserSchema of user model

--- database/user.model.ts
+++ database/user.model.ts
@@ -2,10 +2,17 @@ import { model, models, Schema } from 'mongoose'
 
 const UserSchema = new Schema(
 	{
-		fullName: { type: String },
-		clerkId: { type: String },
-		email: { type: String },
-		picture: { type: String },
+		fullName: String,
+		clerkId: String,
+		email: String,
+		picture: String,
+		bio: String,
+		phone: String,
+		job: String,
+		website: String,
+		linkedin: String,
+		github: String,
+		youtube: String,
 	},
 	{ timestamps: true }
 )
