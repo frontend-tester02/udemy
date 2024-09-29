0ea306729f667be472eb1ae884e4301a35531fa3
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-09-29 22:21:24 +0900

    Updated overview and main page

--- app/[lng]/(root)/courses/[slug]/page.tsx
+++ app/[lng]/(root)/courses/[slug]/page.tsx
@@ -12,7 +12,8 @@ import {
 } from '@/components/ui/carousel'
 import CourseCard from '@/components/cards/course.card'
 import { translation } from '@/i18n/server'
-import { getDetailedCourse } from '@/actions/course.action'
+import { getDetailedCourse, getFeaturedCourses } from '@/actions/course.action'
+import { ICourse } from '@/app.types'
 
 interface Props {
 	params: {
@@ -25,8 +26,10 @@ async function Page({ params: { lng, slug } }: Props) {
 	const { t } = await translation(lng)
 
 	const courseJSON = await getDetailedCourse(slug)
+	const coursesJSON = await getFeaturedCourses()
 
 	const course = JSON.parse(JSON.stringify(courseJSON))
+	const courses = JSON.parse(JSON.stringify(coursesJSON))
 
 	return (
 		<>
@@ -48,9 +51,9 @@ async function Page({ params: { lng, slug } }: Props) {
 					{t('youMayLike')}
 				</h1>
 
-				{/* <Carousel opts={{ align: 'start' }} className='mt-6 w-full'>
+				<Carousel opts={{ align: 'start' }} className='mt-6 w-full'>
 					<CarouselContent className='w-full'>
-						{courses.map(course => (
+						{courses.map((course: ICourse) => (
 							<CarouselItem
 								key={course.title}
 								className='md:basis-1/2 lg:basis-1/3'
@@ -61,7 +64,7 @@ async function Page({ params: { lng, slug } }: Props) {
 					</CarouselContent>
 					<CarouselPrevious />
 					<CarouselNext />
-				</Carousel> */}
+				</Carousel>
 			</div>
 		</>
 	)
