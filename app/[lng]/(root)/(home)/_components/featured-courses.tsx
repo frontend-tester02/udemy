9e2412ff7ef84336ed2398fad649d48c5128c733
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-10-02 22:52:24 +0900

    Add onUpdateParams

--- app/[lng]/(root)/(home)/_components/featured-courses.tsx
+++ app/[lng]/(root)/(home)/_components/featured-courses.tsx
@@ -12,16 +12,29 @@ import {
 } from '@/components/ui/carousel'
 import { filterCourses } from '@/constants'
 import useTranslate from '@/hooks/use-translate'
-import { cn } from '@/lib/utils'
-import { useState } from 'react'
+import { cn, formUrlQuery } from '@/lib/utils'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 interface Props {
 	courses: ICourse[]
 }
 
 function FeaturedCourses({ courses }: Props) {
-	const [filter, setFilter] = useState('all')
 	const t = useTranslate()
+
+	const searchParams = useSearchParams()
+	const router = useRouter()
+
+	const onUpdateParams = (value: string) => {
+		const newUrl = formUrlQuery({
+			value,
+			key: 'filter',
+			params: searchParams.toString(),
+			toCourses: true,
+		})
+
+		router.push(newUrl)
+	}
 
 	return (
 		<div className='container mx-auto max-w-6xl py-12'>
@@ -39,12 +52,12 @@ function FeaturedCourses({ courses }: Props) {
 					{filterCourses.map(item => (
 						<Button
 							key={item.name}
-							variant={filter === item.name ? 'secondary' : 'ghost'}
+							variant={item.name === 'all' ? 'secondary' : 'ghost'}
 							className={cn(
 								'font-medium max-md:w-full max-md:bg-secondary rounded-full',
-								filter === item.name && 'text-primary'
+								item.name === 'all' && 'text-primary'
 							)}
-							onClick={() => setFilter(item.name)}
+							onClick={() => onUpdateParams(item.name)}
 						>
 							{t(item.label)}
 						</Button>
