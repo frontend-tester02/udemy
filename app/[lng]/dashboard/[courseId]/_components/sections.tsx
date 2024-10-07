d4cc49b5b04cfbfd936a71f6ce85edb99a8e5d8c
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-10-07 17:16:07 +0900

    Add SectionList of sections

--- app/[lng]/dashboard/[courseId]/_components/sections.tsx
+++ app/[lng]/dashboard/[courseId]/_components/sections.tsx
@@ -9,3 +9,30 @@ function Sections({ sections }: Props) {
 }
 
 export default Sections
+
+function SectionList(section: ISection) {
+	const searchParams = useSearchParams()
+	const sectionId = searchParams.get('s')
+	return (
+		<AccordionItem value={section._id} className='mt-1'>
+			<AccordionTrigger
+				className={cn(
+					'text-left hover:no-underline hover:bg-gray-50 hover:dark:bg-gray-800 px-3 bg-gray-100 dark:bg-black/20',
+					sectionId === section._id && 'bg-white dark:bg-gray-800'
+				)}
+			>
+				{section.title}
+			</AccordionTrigger>
+			<AccordionContent>
+				{section.lessons.map(lesson => (
+					<LessonList
+						key={lesson._id}
+						lesson={lesson}
+						sectionId={section._id}
+					/>
+				))}
+			</AccordionContent>
+		</AccordionItem>
+	)
+}
+
