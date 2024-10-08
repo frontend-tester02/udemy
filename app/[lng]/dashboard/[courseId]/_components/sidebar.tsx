b66c606aec67d9194c0ad889c94a07f062b62fa8
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-10-08 21:29:09 +0900

    Fixed progressPercentage completion

--- app/[lng]/dashboard/[courseId]/_components/sidebar.tsx
+++ app/[lng]/dashboard/[courseId]/_components/sidebar.tsx
@@ -26,7 +26,7 @@ async function Sidebar({ courseId, lng }: Props) {
 				<h1 className='line-clamp-1 text-xl font-medium'>{course.title}</h1>
 				<Progress value={progressPercentage} className='h-4' />
 				<p className='text-sm'>
-					{progressPercentage}% {t('completed')}{' '}
+					{progressPercentage.toFixed(0)}% {t('completed')}{' '}
 				</p>
 			</div>
 
