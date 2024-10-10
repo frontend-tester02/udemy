e5b5403e75a28b2ff7d7e92f9d5b9f71fd66c90f
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-10-10 13:59:53 +0900

    Add course videos page to show course videos

--- app/[lng]/dashboard/[courseId]/[lessonId]/page.tsx
+++ app/[lng]/dashboard/[courseId]/[lessonId]/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { getLesson } from '@/actions/lesson.action'
+import { translation } from '@/i18n/server'
+import parse from 'html-react-parser'
+import VideoLesson from './_components/video-lesson'
 
-function Page() {
-	return <div>Page</div>
+interface Props {
+	params: { lessonId: string; courseId: string; lng: string }
+}
+async function Page({ params: { lessonId, courseId, lng } }: Props) {
+	const { t } = await translation(lng)
+	const lesson = await getLesson(lessonId)
+	return (
+		<>
+			<VideoLesson lesson={JSON.parse(JSON.stringify(lesson))} />
+			{lesson.content && (
+				<div className='rounded-md bg-gradient-to-b from-background to-secondary px-4 pb-4 pt-1 md:px-8'>
+					<h1 className='mb-2 font-spaceGrotesk text-xl font-medium text-primary'>
+						{t('usefullInformation')}
+					</h1>
+					<div className='prose max-w-none flex-1 dark:prose-invert'>
+						{parse(lesson.content)}
+					</div>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default Page
