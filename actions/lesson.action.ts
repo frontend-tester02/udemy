a84b1b51ed4ddcf16557f089f6447c84a499b362
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-10-14 22:45:25 +0900

    Add toString method to lessonId and sectionId

--- actions/lesson.action.ts
+++ actions/lesson.action.ts
@@ -162,7 +162,10 @@ export const getNextLesson = async (lessonId: string, courseId: string) => {
 		const nextLesson = lessons[lessonIndex + 1]
 
 		const section = await Section.findOne({ lessons: nextLesson._id })
-		return { lessonId: nextLesson._id, sectionId: section._id }
+		return {
+			lessonId: nextLesson._id.toString(),
+			sectionId: section._id.toString(),
+		}
 	} catch (error) {
 		throw new Error('Something went wrong!')
 	}
@@ -193,16 +196,16 @@ export const getLastLesson = async (clerkId: string, courseId: string) => {
 
 		if (!lastLesson) {
 			return {
-				sectionId: sections[0]._id,
-				lessonId: sections[0].lessons[0]._id,
+				sectionId: sections[0]._id.toString(),
+				lessonId: sections[0].lessons[0]._id.toString(),
 			}
 		}
 
 		const section = await Section.findOne({ lessons: lastLesson.lessonId })
 
 		return {
-			lessonId: lastLesson.lessonId,
-			sectionId: section._id,
+			lessonId: lastLesson.lessonId.toString(),
+			sectionId: section._id.toString(),
 		}
 	} catch (error) {
 		throw new Error('Something went wrong!')
