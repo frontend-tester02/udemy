81a61592c5672e88aa5b0a3e6db623615cd11bd1
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-11-03 00:21:46 +0900

    Add next version adn updated review model

--- database/review.model.ts
+++ database/review.model.ts
@@ -1,16 +1,14 @@
-import { model, models, Schema } from 'mongoose'
+import { Schema, models, model } from 'mongoose'
 
 const ReviewSchema = new Schema(
 	{
 		data: String,
 		rating: Number,
 		user: { type: Schema.Types.ObjectId, ref: 'User' },
-		courses: { type: Schema.Types.ObjectId, ref: 'Course' },
+		course: { type: Schema.Types.ObjectId, ref: 'Course' },
 		isFlag: { type: Boolean, default: false },
 	},
-	{
-		timestamps: true,
-	}
+	{ timestamps: true }
 )
 
 const Review = models.Review || model('Review', ReviewSchema)
