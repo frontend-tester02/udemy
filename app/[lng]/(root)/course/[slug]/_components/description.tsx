060194ed361d27fb9cc9e4b984476b3e1d15b185
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-11-13 03:10:57 +0900

    Update buttons to buy

--- app/[lng]/(root)/course/[slug]/_components/description.tsx
+++ app/[lng]/(root)/course/[slug]/_components/description.tsx
@@ -13,32 +13,30 @@ import {
 import { GrCertificate } from 'react-icons/gr'
 import { BiCategory } from 'react-icons/bi'
 import { useState } from 'react'
-import { purchaseCourse } from '@/actions/course.action'
 import { useAuth } from '@clerk/nextjs'
-import { useParams, useRouter } from 'next/navigation'
-import { toast } from 'sonner'
-import FillLoading from '@/components/shared/fill-loading'
+import { useRouter } from 'next/navigation'
+import { useCart } from '@/hooks/use-cart'
+import Link from 'next/link'
 
-function Description(course: ICourse) {
+interface Props {
+	course: ICourse
+	isPurchase: boolean
+}
+
+function Description({ course, isPurchase }: Props) {
 	const [isLoading, setIsLoading] = useState(false)
 
 	const { userId } = useAuth()
 	const t = useTranslate()
 	const router = useRouter()
-	const { lng } = useParams()
+	const { addToCart } = useCart()
 
-	const onPurchase = async () => {
+	const onCart = () => {
 		setIsLoading(true)
-		const promise = purchaseCourse(course._id, userId!)
-			.then(() => router.push(`/${lng}/dashboard/${course._id}`))
-			.catch(() => setIsLoading(false))
-
-		toast.promise(promise, {
-			loading: t('loading'),
-			success: t('successfully'),
-			error: t('error'),
-		})
+		addToCart(course)
+		router.push('/shopping/cart')
 	}
+
 	return (
 		<div className='rounded-md border bg-secondary/50 p-4 shadow-lg dark:shadow-white/20 lg:sticky lg:top-24 lg:p-6'>
 			<div className='flex items-center justify-between font-spaceGrotesk'>
@@ -56,18 +54,28 @@ function Description(course: ICourse) {
 				</div>
 			</div>
 
-			<Button size={'lg'} className='mt-4 w-full font-bold'>
-				{t('addToCart')}
-			</Button>
+			{isPurchase ? (
+				<Button size={'lg'} className='relative mt-2 w-full font-bold' asChild>
+					<Link href={`/dashboard/${course._id}`}>{t('toLesson')}</Link>
+				</Button>
+			) : (
+				<Button
+					size={'lg'}
+					className='relative mt-2 w-full font-bold'
+					disabled={isLoading}
+					onClick={onCart}
+				>
+					{t('buyNow')}
+				</Button>
+			)}
+
 			<Button
 				size={'lg'}
 				className='relative mt-2 w-full font-bold'
 				variant={'outline'}
 				disabled={isLoading}
-				onClick={onPurchase}
 			>
-				{isLoading && <FillLoading />}
-				{t('buyNow')}
+				{t('addWishlist')}
 			</Button>
 
 			<p className='my-3 text-center text-sm text-muted-foreground'>
