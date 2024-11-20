601467bd96b80a713a825334a8f340f9cb84f177
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-11-21 02:38:26 +0900

    Updated courses pagination searchParams

--- app/[lng]/(root)/courses/page.tsx
+++ app/[lng]/(root)/courses/page.tsx
@@ -5,9 +5,9 @@ import { SearchParamsProps } from '@/app.types'
 
 async function Page({ searchParams }: SearchParamsProps) {
 	const resultJSON = await getAllCourses({
-		page: searchParams.page ? +searchParams : 1,
-		filter: searchParams.filter,
 		searchQuery: searchParams.q,
+		filter: searchParams.filter,
+		page: searchParams.page ? +searchParams.page : 1,
 	})
 
 	const result = JSON.parse(JSON.stringify(resultJSON))
