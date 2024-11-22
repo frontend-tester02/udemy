3ac1621670949f442156e058c48fc682dd4b0e45
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-11-22 23:41:44 +0900

    Activate admin instructors page

---
+++ app/[lng]/admin/instructors/_components/instructor-item.tsx
@@ -0,0 +1,141 @@
+'use client'
+
+import { updateUser } from '@/actions/user.action'
+import { IUser } from '@/app.types'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { TableCell, TableRow } from '@/components/ui/table'
+import { MoreHorizontal } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { toast } from 'sonner'
+
+interface Props {
+	instructor: IUser
+}
+
+function InstructorItem({ instructor }: Props) {
+	const pathname = usePathname()
+
+	const onRoleChange = async () => {
+		const msg = instructor.role === 'instructor' ? 'Disapprove' : 'Approve'
+
+		const isConfirmed = confirm(`Aru you sure you want to ${msg} this user?`)
+
+		if (isConfirmed) {
+			const promise = updateUser({
+				clerkId: instructor.clerkId,
+				updatedData: {
+					role: instructor.role === 'user' ? 'instructor' : 'user',
+				},
+				path: pathname,
+			})
+
+			toast.promise(promise, {
+				loading: 'Loading...',
+				success: `${msg} successfully!`,
+				error: 'Something went wrong. Please try again.',
+			})
+		}
+	}
+
+	const onAdmin = async () => {
+		const isConfirmed = confirm(
+			`Aru you sure you want to make this user an admin?`
+		)
+
+		if (isConfirmed) {
+			const promise = updateUser({
+				clerkId: instructor.clerkId,
+				updatedData: {
+					isAdmin: true,
+				},
+				path: pathname,
+			})
+
+			toast.promise(promise, {
+				loading: 'Loading...',
+				success: `Successfully!`,
+				error: 'Something went wrong. Please try again.',
+			})
+		}
+	}
+
+	const onDelete = async () => {
+		const isConfirmed = confirm(
+			`Aru you sure you want to delete this instructor?`
+		)
+
+		if (isConfirmed) {
+			const promise = updateUser({
+				clerkId: instructor.clerkId,
+				updatedData: {
+					approvedInstructor: false,
+					role: 'user',
+				},
+				path: pathname,
+			})
+
+			toast.promise(promise, {
+				loading: 'Loading...',
+				success: `Successfully!`,
+				error: 'Something went wrong. Please try again.',
+			})
+		}
+	}
+
+	return (
+		<TableRow>
+			<TableCell className='text-xs capitalize'>
+				{instructor.isAdmin ? 'Admin/' : ''}
+				{instructor.role}
+			</TableCell>
+			<TableCell className='text-xs'>{instructor.email}</TableCell>
+			<TableCell
+				className='cursor-pointer text-xs text-primary hover:underline'
+				onClick={() => window.open(instructor.website, '_blank')}
+			>
+				{instructor.website.replace(/^https?:\/\//, '')}
+			</TableCell>
+			<TableCell
+				className='cursor-pointer text-xs text-primary hover:underline'
+				onClick={() => window.open(instructor.youtube, '_blank')}
+			>
+				{instructor.youtube.replace(/^https?:\/\//, '')}
+			</TableCell>
+			<TableCell
+				className='cursor-pointer text-xs text-primary hover:underline'
+				onClick={() => window.open(instructor.github, '_blank')}
+			>
+				{instructor.github.replace(/^https?:\/\//, '')}
+			</TableCell>
+			<TableCell className='text-xs'>{instructor.job}</TableCell>
+			<TableCell className='text-right'>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button size={'icon'} variant={'ghost'}>
+							<MoreHorizontal className='size-6' />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>Manage</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem onClick={onRoleChange}>
+							{instructor.role === 'instructor' ? 'Disapprove' : 'Approve'}
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={onAdmin}>Admin</DropdownMenuItem>
+						<DropdownMenuItem onClick={onDelete}>Delete</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</TableCell>
+		</TableRow>
+	)
+}
+
+export default InstructorItem
--- app/[lng]/admin/instructors/page.tsx
+++ app/[lng]/admin/instructors/page.tsx
@@ -1,5 +1,48 @@
-function Page() {
-	return <div>Page</div>
+import { getInstructors } from '@/actions/user.action'
+import Header from '@/components/shared/header'
+import {
+	Table,
+	TableBody,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+import InstructorItem from './_components/instructor-item'
+
+async function Page() {
+	const instructors = await getInstructors()
+
+	return (
+		<>
+			<Header
+				title='Instructors'
+				description='Approve or disapprove them. 
+				You can also give them the admin role.'
+			/>
+
+			<Table className='mt-4 bg-background'>
+				<TableHeader>
+					<TableRow>
+						<TableHead className='w-[100px]'>Role</TableHead>
+						<TableHead className='w-[100px]'>Email</TableHead>
+						<TableHead className='w-[100px]'>Portfolio</TableHead>
+						<TableHead className='w-[100px]'>YoouTube</TableHead>
+						<TableHead className='w-[100px]'>GitHub</TableHead>
+						<TableHead>Job</TableHead>
+						<TableHead className='text-right'>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{instructors.map(instructor => (
+						<InstructorItem
+							key={instructor._id}
+							instructor={JSON.parse(JSON.stringify(instructor))}
+						/>
+					))}
+				</TableBody>
+			</Table>
+		</>
+	)
 }
 
 export default Page
